d3b999109154b70891ae8452b5036a75b52261e5
solar <nathf1005@naver.com>
2025-01-14 00:23:46 +0900

    feat: 갤러리 이미지 삭제 기능 추가 및 추가 버튼 표시

--- app/index.js
+++ app/index.js
@@ -1,30 +1,60 @@
 import useGallery from "@/src/use-gallery";
 import {
-  Button,
+  Dimensions,
   FlatList,
   Image,
   Platform,
   SafeAreaView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 
+const width = Dimensions.get("screen").width;
+const columnSize = width / 3;
+
 export default function Index() {
-  const { images, pickImage } = useGallery();
+  const { images, pickImage, deleteImage, imagesWithAddButton } = useGallery();
   const onPressOpenGallery = () => {
     pickImage();
   };
+  const onLongPressImage = (imagesId) => deleteImage(imagesId);
 
-  const renderItem = ({ item }) => {
-    return <Image source={{ uri: item }} style={{ width: 200, height: 200 }} />;
+  const renderItem = ({ item: { id, uri }, index }) => {
+    if (id === -1) {
+      return (
+        <TouchableOpacity
+          onPress={onPressOpenGallery}
+          style={{
+            width: columnSize,
+            height: columnSize,
+            backgroundColor: "lightgray",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontWeight: "100", fontSize: 45 }}>+</Text>
+        </TouchableOpacity>
+      );
+    }
+    return (
+      <TouchableOpacity onLongPress={() => onLongPressImage(id)}>
+        <Image
+          source={{ uri: uri }}
+          style={{ width: columnSize, height: columnSize }}
+        />
+      </TouchableOpacity>
+    );
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Button title="갤러리 열기" onPress={onPressOpenGallery} />
-
-      <FlatList data={images} renderItem={renderItem} />
+      <FlatList
+        data={imagesWithAddButton}
+        renderItem={renderItem}
+        numColumns={3}
+      />
     </SafeAreaView>
   );
 }
