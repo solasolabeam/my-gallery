9bfcdb87612e7bf06db1b667c1ea05dbd7fd3ea1
solar <nathf1005@naver.com>
2025-01-15 01:25:02 +0900

    feat: 앨범 선택 기능 추가 및 앨범 추가 모달 구현

--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
+import MyDropdownPicker from "@/src/MyDropdownPicker";
+import TextInputModal from "@/src/TextInputModal";
 import useGallery from "@/src/use-gallery";
 import {
   Dimensions,
@@ -15,12 +17,22 @@ const width = Dimensions.get("screen").width;
 const columnSize = width / 3;
 
 export default function Index() {
-  const { images, pickImage, deleteImage, imagesWithAddButton } = useGallery();
+  const {
+    pickImage,
+    deleteImage,
+    imagesWithAddButton,
+    selectedAlbum,
+    modalVisible,
+    openModal,
+    closeModal,
+  } = useGallery();
   const onPressOpenGallery = () => {
     pickImage();
   };
   const onLongPressImage = (imagesId) => deleteImage(imagesId);
-
+  const onPressAddAlbum = () => {
+    openModal();
+  };
   const renderItem = ({ item: { id, uri }, index }) => {
     if (id === -1) {
       return (
@@ -50,6 +62,15 @@ export default function Index() {
 
   return (
     <SafeAreaView style={styles.container}>
+      {/* 앨범 Dropdown, 앨범 추가 버튼, 이미지 목록 */}
+      <MyDropdownPicker
+        selectedAlbumTitle={selectedAlbum.title}
+        onPressAddAlbum={onPressAddAlbum}
+      />
+
+      {/* 앨범을 추가하는 TextInputModal */}
+      <TextInputModal modalVisible={modalVisible} />
+      {/* 이미지 리스트 */}
       <FlatList
         data={imagesWithAddButton}
         renderItem={renderItem}
