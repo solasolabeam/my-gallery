d3a300bfdba97255b777bf59cc0431c0bda051d1
solar <nathf1005@naver.com>
2025-01-15 23:37:25 +0900

    feat: 텍스트 입력 모달에 앨범 제목 입력 기능 추가

--- src/TextInputModal.js
+++ src/TextInputModal.js
@@ -1,9 +1,51 @@
-import { Modal, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Modal,
+  Platform,
+  Pressable,
+  SafeAreaView,
+  TextInput,
+  View,
+} from "react-native";
 
-export default ({ modalVisible }) => {
+export default ({
+  modalVisible,
+  albumTitle,
+  setAlbumTitle,
+  onSubmitEditing,
+  onPressBackdrop,
+}) => {
   return (
     <Modal animationType="slide" transparent={true} visible={modalVisible}>
-      <View style={{ flex: 1, backgroundColor: "lightgreen" }}></View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <Pressable onPress={onPressBackdrop} style={{ flex: 1 }}>
+          <SafeAreaView
+            style={{
+              flex: 1,
+              width: "100%",
+              position: "absolute",
+              bottom: 0,
+            }}
+          >
+            <TextInput
+              placeholder="앨범명을 입력해주세요"
+              style={{
+                width: "100%",
+                padding: 10,
+                borderWidth: 0.5,
+                borderColor: "lightgrey",
+              }}
+              value={albumTitle}
+              onChangeText={setAlbumTitle}
+              onSubmitEditing={onSubmitEditing}
+              autoFocus={true}
+            />
+          </SafeAreaView>
+        </Pressable>
+      </KeyboardAvoidingView>
     </Modal>
   );
 };
